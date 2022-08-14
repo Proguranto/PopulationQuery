d496870d11fdaf6056adf0f888129430bbbf43ad
nilakeri <nilakeri@cs.uw.edu>
2022-08-14 10:49:09 -0700

    Writeup Experiment Tests done

---
+++ src/test/java/ckpt1/WriteUpTests.java
@@ -0,0 +1,70 @@
+package ckpt1;
+
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+import cse332.interfaces.QueryResponder;
+import cse332.types.CensusGroup;
+import base.QueryResponderTests;
+import queryresponders.*;
+
+
+public class WriteUpTests extends QueryResponderTests {
+
+    @Test
+    public void getPopSimpleSequential() {
+        long totalTime = 0;
+        long avgTime;
+        Random random = new Random();
+        random.setSeed(2548);
+        int n = random.nextInt(500) + 1;
+        int s = random.nextInt(n) + 1;
+        int e = random.nextInt(100) + 1;
+        int w = random.nextInt(e) + 1;
+        System.out.printf("%d,%d,%d,%d\n",n,s,e,w);
+        for (int i = 0; i < 15; i++) {
+            long startTimer = System.nanoTime();
+            CensusGroup[] data;
+            data = readCensusdata();
+            QueryResponder SimpleSequential = new SimpleSequential(data, 100, 500);
+            for (int j = 0; j < 2; j++) {
+                SimpleSequential.getPopulation(w, s, e, n);
+            }
+            long endTimer = System.nanoTime();
+            // Eliminate 5 warm up runs
+            if (i >= 5) {
+                totalTime += (endTimer - startTimer);
+            }
+        }
+        avgTime = totalTime / 10;
+        System.out.println("Simple Sequential avg time: " + avgTime  + " ns");
+    }
+
+//    @Test
+//    public void getPopComplexSequential() {
+//        long totalTime = 0;
+//        long avgTime = 0;
+//        Random random = new Random();
+//        random.setSeed(2548);
+//        int n = random.nextInt(500) + 1;
+//        int s = random.nextInt(n) + 1;
+//        int e = random.nextInt(100) + 1;
+//        int w = random.nextInt(e) + 1;
+//        System.out.printf("%d,%d,%d,%d\n",n,s,e,w);
+//        for (int i = 0; i < 15; i++) { //warm up + actual timing
+//            long startTimer = System.nanoTime();
+//            CensusGroup[] data;
+//            data = readCensusdata();
+//            QueryResponder ComplexSequential = new ComplexSequential(data, 100, 500);
+//            for (int j = 0; j < 1; j++) {
+//                ComplexSequential.getPopulation(w, s, e, n);
+//            }
+//            long endTimer = System.nanoTime();
+//            // Eliminate 5 warm up runs
+//            if (i >= 5) {
+//                totalTime += (endTimer - startTimer);
+//            }
+//        }
+//        avgTime = totalTime / 10;
+//        System.out.println("Complex Sequential avg time: " + avgTime + " ns");
+//    }
+}
